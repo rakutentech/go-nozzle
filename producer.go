4989cc8c32afd0d847537f9ce6a9032622e8d6fe
tcnksm <nsd22843@gmail.com>
2015-11-06 16:45:33 +0900

    Remove producer

--- producer.go
+++
@@ -1,7 +0,0 @@
-package nozzle
-
-import "github.com/cloudfoundry/sonde-go/events"
-
-type Producer interface {
-	Produce(chan *events.Envelope, chan error, SlowDetectCh)
-}
--- producer_test.go
+++
@@ -1,7 +0,0 @@
-package nozzle
-
-import "github.com/cloudfoundry/sonde-go/events"
-
-type FakeProducer struct{}
-
-func (p *FakeProducer) Produce(_ chan *events.Envelope, _ chan error, _ SlowDetectCh) {}
